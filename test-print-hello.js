dc8b41acfd14d729b8ea396142e16eba09343db2
Nirmala Tajne <nirmala.tajne@syndigo.com>
2022-02-25 18:41:47 +0530

    Post commit

--- test-print-hello.js
+++ test-print-hello.js
@@ -1,1 +1,4 @@
+let fs = require('fs')
 console.log("Hello I am from test-print-hello")
+console.log('Post commit');
+fs.createWriteStream('D:/riversand-thoughts-implementation/testHooks/AutomationRunner/test.txt');
