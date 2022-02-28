a930b4cb5c5dddd2aa9397a754f59ab76df15353
Nirmala Tajne <nirmala.tajne@syndigo.com>
2022-02-28 18:10:59 +0530

    Update test-print-hello.js

--- test-print-hello.js
+++ test-print-hello.js
@@ -3,3 +3,4 @@ const path = require('path')
 console.log("Hello I am from test-print-hello")
 console.log('Post commit');
 fs.createWriteStream(`${__dirname}/test.txt`);
+console.log('Created file location '+`${__dirname}/test.txt`);
